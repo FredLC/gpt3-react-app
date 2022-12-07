9895164a3c4500614e26f9d1846b53436135c9ec
Fred Lefebvre <fred@MacBook-Air-de-Christine.local>
2022-12-07 12:43:25 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import {
+  Blog,
+  Features,
+  Footer,
+  Header,
+  Possibility,
+  WhatGPT3,
+} from "./containers";
+import { CTA, Brand, Navbar } from "./components";
+
+const App = () => {
+  return (
+    <div>
+      <h1>GPT-3</h1>
+    </div>
+  );
+};
+
+export default App;
